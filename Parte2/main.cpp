5fd1e4485f1fd502964726b099b526f5a77f4d65
MarioGonzalo <146821965+MarioGonzalo@users.noreply.github.com>
2023-12-05 11:11:09 +0100

    Apartado 2 parte 2

--- Parte2/main.cpp
+++ Parte2/main.cpp
@@ -13,11 +13,6 @@ public:
     // Destructor
     ~Environment() {}
 
-    // Función para agregar una variable al entorno
-    void addVariable(const string& name, const Variant& value) {
-        symbolTable[name] = value;
-    }
-
     // Función para obtener el valor de una variable en el entorno
     Variant getVariable(const string& name) const {
         auto it = symbolTable.find(name);
@@ -25,10 +20,15 @@ public:
             return it->second;
         } else {
             // Manejo de error: la variable no está definida en el entorno
-            return Variant();
+            cerr << "Error: variable " << name << " no definida" << endl;
         }
     }
-
+    void insertVariable(const string& name, const Variant& value){
+        auto insert = symbolTable.insert(pair<string, Variant>(name, value));
+        if (!insert.second){
+            cerr << "Error: variable " << name << " ya definida" << endl;
+        }
+    }
 private:
     // Tabla de símbolos que asocia nombres de variables con sus valores
     map<string, Variant> symbolTable;
@@ -39,6 +39,12 @@ private:
 
 
 int main() {
+    Environment env;
 
+    env.insertVariable("x", Variant(5));
+    env.insertVariable("y", Variant(3.14f));
+    env.insertVariable("z", Variant("Hola mundo"));
+
+    cout << env.getVariable("x").getInt() << endl;
     return 0;
 }

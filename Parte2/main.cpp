3d0bde2999b46b6c7959b63bb9ebc4df5f4713c8
MarioGonzalo <146821965+MarioGonzalo@users.noreply.github.com>
2023-12-05 11:22:13 +0100

    Apartado 3 parte 2

--- Parte2/main.cpp
+++ Parte2/main.cpp
@@ -21,6 +21,7 @@ public:
         } else {
             // Manejo de error: la variable no está definida en el entorno
             cerr << "Error: variable " << name << " no definida" << endl;
+            return Variant();
         }
     }
     //Inicio apartado 2
@@ -33,6 +34,23 @@ public:
         }
     }
     //Fin apartado 2
+
+    //Inicio apartado 3
+    // Función para modificar el valor de una variable en el entorno
+    Variant lookup(const string& name){
+        auto it = symbolTable.find(name);//Busca la variable en la tabla de símbolos
+        //Si la encuentra, devuelve el valor de la variable
+        if (it != symbolTable.end()) {
+            return it->second;
+
+        } else {
+            //Si no la encuentra, devuelve un mensaje de error
+            cerr << "Variable " << name << " no definida" << endl;
+            return Variant();
+        }
+    }
+    //Fin apartado 3
+
 private:
     // Tabla de símbolos que asocia nombres de variables con sus valores
     map<string, Variant> symbolTable;
@@ -49,6 +67,8 @@ int main() {
     env.insertVariable("x", Variant(5));
     env.insertVariable("y", Variant(3.14f));
     env.insertVariable("z", Variant("Hola mundo"));
+    env.lookup("x").getInt();//Devuelve el valor de la variable x
+    env.lookup("arbol").getInt();//Devuelve un mensaje de error
 
     cout << env.getVariable("x").getInt() << endl;
     return 0;
